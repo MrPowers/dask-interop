9ccaf41cfc039eff664471a66304598bc6ff89fa
MrPowers <matthewkevinpowers@gmail.com>
2021-09-27 07:39:08 -0500

    Refactor script to create Spark generated files

--- resources/create.py
+++ resources/create.py
@@ -1,6 +1,17 @@
+from pathlib import Path
+import shutil
+
 from pyspark.sql import SparkSession
 
 from delta import *
+
+dirpath = Path("resources") / "parquet"
+if dirpath.exists() and dirpath.is_dir():
+    shutil.rmtree(dirpath)
+
+dirpath = Path("resources") / "delta"
+if dirpath.exists() and dirpath.is_dir():
+    shutil.rmtree(dirpath)
 
 builder = (
     SparkSession.builder.appName("dask-interop")
@@ -9,6 +20,7 @@ builder = (
         "spark.sql.catalog.spark_catalog",
         "org.apache.spark.sql.delta.catalog.DeltaCatalog",
     )
+    .config("spark.databricks.delta.schema.autoMerge.enabled", True)
 )
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
@@ -19,6 +31,29 @@ df = spark.createDataFrame(data, ["name", "num"])
 df.write.mode("overwrite").parquet("resources/parquet/1")
 
 # write Delta files with PySpark
+
+# 1
 data = [("jose", 10), ("li", 12), ("luisa", 14)]
 df = spark.createDataFrame(data, ["name", "num"])
-df.write.mode("overwrite").format("delta").save("resources/delta/1")
+df.write.format("delta").save("resources/delta/1")
+
+# 2
+data = [("a", 1), ("b", 2), ("c", 3)]
+df = spark.createDataFrame(data, ["letter", "number"])
+df.write.format("delta").save("resources/delta/2")
+
+data = [("d", 4), ("e", 5), ("f", 6)]
+df = spark.createDataFrame(data, ["letter", "number"])
+df.write.mode("append").format("delta").save("resources/delta/2")
+
+# 3
+data = [("a", 1), ("b", 2), ("c", 3)]
+df = spark.createDataFrame(data, ["letter", "number"])
+df.write.format("delta").save("resources/delta/3")
+
+data = [("d", 4, "red"), ("e", 5, "blue"), ("f", 6, "green")]
+df = spark.createDataFrame(data, ["letter", "number", "color"])
+df.write.mode("append").format("delta").save("resources/delta/3")
+
+df = spark.read.format("delta").option("mergeSchema", "true").load("resources/delta/3")
+df.show()
--- tests/test_delta.py
+++ tests/test_delta.py
@@ -1,8 +1,8 @@
 import pytest
 import os
 import beavis
-import dask.dataframe as dd
 import pandas as pd
+import dask.dataframe as dd
 from deltalake import DeltaTable
 
 
@@ -14,3 +14,16 @@ def test_pyspark_delta_to_dask():
     df = pd.DataFrame({"name": ["jose", "li", "luisa"], "num": [10, 12, 14]})
     expected_ddf = dd.from_pandas(df, npartitions=1)
     beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False)
+
+
+def test_delta_timetravel():
+    dt = DeltaTable("resources/delta/2")
+    dt.load_version(0)
+    filenames = ["resources/delta/2/" + f for f in dt.files()]
+
+    actual_ddf = dd.read_parquet(filenames, engine="pyarrow")
+    print("***")
+    print(actual_ddf.compute())
+    # df = pd.DataFrame({"name": ["jose", "li", "luisa"], "num": [10, 12, 14]})
+    # expected_ddf = dd.from_pandas(df, npartitions=1)
+    # beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False)
